58d66586ce34165df34d7f356da5479e83051520
carolinesales <caroline.vsales@gmail.com>
2026-06-17 20:25:09 -0300

    feat: consolida Perfil em página única com fluxo de e-mail seguro

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Splash                 from './pages/Splash'
 import Login                  from './pages/Login'
 import Cadastro               from './pages/Cadastro'
 import VerificarEmail         from './pages/VerificaEmail'
+import EsqueceuSenha          from './pages/EsqueceuSenha'
 import Questionario           from './pages/Questionario'
 import Analisando             from './pages/Analisando'
 import Resultado              from './pages/Resultado'
@@ -16,10 +17,6 @@ import Analises               from './pages/Analises'
 import Reavaliacao            from './pages/Reavaliacao'
 import JornadaCapilar         from './pages/JornadaCapilar'
 import PerfilMenu             from './pages/perfil/PerfilMenu'
-import PerfilPessoal          from './pages/perfil/PerfilPessoal'
-import PerfilNotificacoes     from './pages/perfil/PerfilNotificacoes'
-import PerfilIdioma           from './pages/perfil/PerfilIdioma'
-import PerfilSeguranca        from './pages/perfil/PerfilSeguranca'
 
 export default function App() {
   return (
@@ -32,6 +29,7 @@ export default function App() {
             <Route path="/login"           element={<Login />} />
             <Route path="/cadastro"        element={<Cadastro />} />
             <Route path="/verificar-email" element={<VerificarEmail />} />
+            <Route path="/esqueceu-senha"  element={<EsqueceuSenha />} />
 
             {/* ── Diagnóstico ── */}
             <Route path="/questionario" element={<ProtectedRoute><Questionario /></ProtectedRoute>} />
@@ -48,10 +46,6 @@ export default function App() {
 
             {/* ── Perfil ── */}
             <Route path="/app/perfil"              element={<ProtectedRoute><PerfilMenu /></ProtectedRoute>} />
-            <Route path="/app/perfil/pessoal"      element={<ProtectedRoute><PerfilPessoal /></ProtectedRoute>} />
-            <Route path="/app/perfil/notificacoes" element={<ProtectedRoute><PerfilNotificacoes /></ProtectedRoute>} />
-            <Route path="/app/perfil/idioma"       element={<ProtectedRoute><PerfilIdioma /></ProtectedRoute>} />
-            <Route path="/app/perfil/seguranca"    element={<ProtectedRoute><PerfilSeguranca /></ProtectedRoute>} />
 
             {/* ── Fallback ── */}
             <Route path="*" element={<Navigate to="/" replace />} />
